e17063f2ac867e0b93cb86ba7aee18bdd013ecdd
Aleksdip <aleks.viunoff@gmail.com>
2023-06-18 20:41:20 +0300

    added task 2

--- src/task_01/car/Car.java
+++ src/task_01/car/Car.java
@@ -2,17 +2,41 @@ package task_01.car;
 
 public class Car {
     int age;
+    double speed;
+    int weight;
     String color;
+
 
     public Car() {
     }
 
     public Car(int age) {
         this.age = age;
+        this.speed = 0;
+        this.weight = 0;
+        this.color = "black";
     }
 
     public Car(int age, String color) {
         this.age = age;
         this.color = color;
+        this.speed = 0;
+        this.weight = 0;
+
     }
+
+    public Car(int age, double speed, String color) {
+        this.age = age;
+        this.speed = speed;
+        this.color = color;
+        this.weight = 0;
+    }
+
+    public Car(int age, double speed, int weight, String color) {
+        this.age = age;
+        this.speed = speed;
+        this.weight = weight;
+        this.color = color;
+    }
+
 }
--- src/task_01/car/Main.java
+++ src/task_01/car/Main.java
@@ -2,8 +2,10 @@ package task_01.car;
 
 public class Main {
     public static void main(String[] args) {
-        Car bmw = new Car();
         Car ford = new Car(5);
         Car opel = new Car(10, "white");
+        Car mercedes = new Car(1,220.5,"red");
+        Car tesla = new Car(3,199.99, 1550, "green");
+
     }
 }
