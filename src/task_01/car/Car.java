b2cbf5c99adbeb275192abaa5f094ee92942af4a
Aleksdip <aleks.viunoff@gmail.com>
2023-06-18 15:25:51 +0300

    added the task 01

---
+++ src/task_01/car/Car.java
@@ -0,0 +1,18 @@
+package task_01.car;
+
+public class Car {
+    int age;
+    String color;
+
+    public Car() {
+    }
+
+    public Car(int age) {
+        this.age = age;
+    }
+
+    public Car(int age, String color) {
+        this.age = age;
+        this.color = color;
+    }
+}
---
+++ src/task_01/car/Main.java
@@ -0,0 +1,9 @@
+package task_01.car;
+
+public class Main {
+    public static void main(String[] args) {
+        Car bmw = new Car();
+        Car ford = new Car(5);
+        Car opel = new Car(10, "white");
+    }
+}
